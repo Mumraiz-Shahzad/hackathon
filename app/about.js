3259d6128917eacbf1b06a0427c3fad9af6c633a
Mumraiz Shahzad <mumraiz1996@gmail.com>
2024-12-09 16:12:53 +0500

    Changes new

---
+++ app/about.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div className="bg-white">
+      <header className="p-4 bg-white">
+        <div className="container mx-auto flex items-center justify-between">
+          <img src="/download%20(1).png" alt="Logo" className="h-18 w-auto" />
+          <nav className="flex bg-black text-yellow-500 font-bold p-6">
+            <ul className="flex space-x-12">
+              <li><Link href="/"><a>Home</a></Link></li>
+              <li><Link href="#"><a>Men's</a></Link></li>
+              <li><Link href="#"><a>Women's</a></Link></li>
+              <li><Link href="#"><a>Kid's</a></Link></li>
+              <li><Link href="/about"><a>About</a></Link></li>
+              <li><Link href="/contact"><a>Contact</a></Link></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main className="container mx-auto mt-6 p-4 bg-black text-yellow-500 shadow-md rounded-md">
+        <h2 className="text-4xl font-semibold mb-4 hover:bg-white hover:text-black">About Us</h2>
+        <p className="text-lg leading-relaxed mb-8">
+          We provide exceptional service and high-quality products, focusing on customer satisfaction.
+        </p>
+        <Link href="/contact">
+          <a className="px-6 py-2 bg-white text-yellow-500 font-semibold rounded hover:bg-black hover:text-white transition">
+            Get in Touch
+          </a>
+        </Link>
+      </main>
+
+      <footer className="bg-black text-yellow-500 hover:bg-white hover:text-black mt-30 text-center px-12 py-4 font-bold">
+        &copy; 2024 ORNI OUTFIT - All Rights Reserved
+      </footer>
+    </div>
+  );
+}
---
+++ app/faq.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function FAQ() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Frequently Asked Questions</h1>
+      <ul className="list-disc pl-6">
+        <li className="mb-4"><strong>Question 1:</strong> Answer 1.</li>
+        <li className="mb-4"><strong>Question 2:</strong> Answer 2.</li>
+        <li><strong>Question 3:</strong> Answer 3.</li>
+      </ul>
+    </div>
+  );
+}
